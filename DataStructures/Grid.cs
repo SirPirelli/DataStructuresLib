555f46b49b619147cbe98bfebcba57999d51b14d
Jose Pereira <jose_miguel_pereira@hotmail.com>
2019-07-05 03:18:28 +0100

    Started creation of a generic grid like structure
    -Added QuadNode.cs
    -Added Grid.cs
    
    Both classes are still barebones.

---
+++ DataStructures/Grid.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by José Pereira
+    04/07/2019
+ */
+
+namespace DataStructures
+{
+    /// <summary>
+    /// Grid-like structure. The entry points are the top of columns.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Grid<T>
+    {
+
+        QuadNode<T>[] _columnsRoots;
+
+        public QuadNode<T> GetNode(QuadNode<T> node)
+        {
+
+            for (int i = 0; i < _columnsRoots.Length; i++)
+            {
+
+                QuadNode<T> queryNode = SearchColumn(_columnsRoots[i], node);
+
+                if (queryNode != null) return queryNode;
+
+            }
+
+            return null;
+        }
+
+        public QuadNode<T> GetNode(QuadNode<T> node, int column)
+        {
+
+            return SearchColumn(_columnsRoots[column], node);
+
+        }
+
+        private QuadNode<T> SearchColumn(QuadNode<T> root, QuadNode<T> node)
+        {
+            if (root == null) return null;
+
+            if (root == node) return root;
+            else return SearchColumn(root.BottomPiece, node);
+        }
+    }
+}
---
+++ DataStructures/QuadNode.cs
@@ -0,0 +1,26 @@
+﻿/* 
+ *  Created by José Pereira
+    04/07/2019
+ */
+
+namespace DataStructures
+{
+    /// <summary>
+    /// QuadTree Node. Only has the neigbours, the all the logic and further data needs to be implemented.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class QuadNode<T>
+    {
+
+        public QuadNode<T> TopPiece { get; set; }
+
+        public QuadNode<T> LeftPiece { get; set; }
+
+        public QuadNode<T> BottomPiece { get; set; }
+
+        public QuadNode<T> RightPiece { get; set; }
+
+    }
+
+    
+}
